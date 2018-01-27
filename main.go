b5f57cffd3a3c5252da583cde90f95e376be6576
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2018-01-27 11:21:13 -0500

    isolating Appdir()

--- main.go
+++ main.go
@@ -8,6 +8,6 @@ import (
 
 func main() {
 	fmt.Println(xplat.Appdir("WhoHaa", "heehee"))
-	err := xplat.Openbrowser("https://fenwickelliott.io")
-	fmt.Println(err)
+	// err := xplat.Openbrowser("https://fenwickelliott.io")
+	// fmt.Println(err)
 }
