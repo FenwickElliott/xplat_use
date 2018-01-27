18215ab63f283daac241ac6d477dbae280db5c6e
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2018-01-27 10:55:24 -0500

    initial Appdir() and Openbrowser()

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/fenwickelliott/xplat"
+)
+
+func main() {
+	fmt.Println(xplat.Appdir("WhoHaa", "heehee"))
+	err := xplat.Openbrowser("https://fenwickelliott.io")
+	fmt.Println(err)
+}
